880ceecd76e33703e29f718428b138e712cac4ba
AndresCastillo28 <ansa2000@hotmail.es>
2024-01-12 19:18:53 -0500

    feat: Implement Signup API for User Management

---
+++ src/main/java/com/optitasolutions/constants/CafeConstants.java
@@ -0,0 +1,8 @@
+package com.optitasolutions.constants;
+
+public class CafeConstants {
+
+    public static final String SOMETHING_WENT_WRONG= "Something Went Wrong.";
+
+    public static final String INVALID_DATA = "INVALID Data.";
+}
