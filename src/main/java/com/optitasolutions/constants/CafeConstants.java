cfa04d6d3a8ca39bd2c694f69ab800470778bef5
AndresCastillo28 <ansa2000@hotmail.es>
2024-01-19 21:10:01 -0500

    feat: Generate Report API of Bill

--- src/main/java/com/optitasolutions/constants/CafeConstants.java
+++ src/main/java/com/optitasolutions/constants/CafeConstants.java
@@ -7,4 +7,6 @@ public class CafeConstants {
     public static final String INVALID_DATA = "INVALID Data.";
 
     public static final String UNAUTHORIZED_ACCESS = "Unauthorized access.";
+
+    public static final String STORE_LOCATION = "C:\\Users\\Andres\\Documents\\document-cafe-management";
 }
---
+++ src/main/java/com/optitasolutions/restImpl/BillRestImpl.java
@@ -0,0 +1,28 @@
+package com.optitasolutions.restImpl;
+
+import com.optitasolutions.constants.CafeConstants;
+import com.optitasolutions.rest.BillRest;
+import com.optitasolutions.service.BillService;
+import com.optitasolutions.utils.CafeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class BillRestImpl implements BillRest {
+
+    @Autowired
+    BillService billService;
+    @Override
+    public ResponseEntity<String> generateReport(Map<String, Object> requestMap) {
+        try {
+            return billService.generateReport(requestMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
